a49de6a5699caace97d1a37bcf8316378a90808a
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-03-24 12:43:12 -0300

    feat: config prettier with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-    <h1 className="text-purple-500">Teste</h1>      
+      <h1 className="text-purple-500">Teste</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
