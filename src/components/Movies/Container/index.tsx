4cd114f3147f062b2bae8d8915929512e19d7d0b
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-03-30 01:15:55 -0300

    feat: added movies Query in component

--- src/components/Movies/Container/index.tsx
+++ src/components/Movies/Container/index.tsx
@@ -1,8 +1,13 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useState } from "react";
+import { useMoviesQuery } from "../../../hooks/query/movies/useMoviesQuery";
 
 type MovieContainer = HTMLAttributes<HTMLDivElement>;
 
 export const MovieContainer = ({ children, ...rest }: MovieContainer) => {
+  const [searchTerm, setSearchTem] = useState("terror");
+
+  const { data } = useMoviesQuery({ search: searchTerm });
+  console.log({ data });
   return (
     <main className="mx-auto max-w-[1320px]" {...rest}>
       {children}
