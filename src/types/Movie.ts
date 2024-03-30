7e4ef57cb2b784516b3f69bf18614b0040eab0f2
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-03-30 12:44:06 -0300

    feat: create Movie types

---
+++ src/types/Movie.ts
@@ -0,0 +1,15 @@
+export type MoviePersistence = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
+
+export type Movie = {
+  title: string;
+  year: string;
+  imdbID: string;
+  type: string;
+  poster: string;
+};
