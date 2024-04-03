467aee8abe6df832ee771516ce4965311aa13592
Cleyton Furtado <cleytonrodrigues69@live.com>
2024-04-02 23:47:40 -0300

    feat: added styles to ModalBody

--- src/components/Modal/Body/index.tsx
+++ src/components/Modal/Body/index.tsx
@@ -3,5 +3,9 @@ import { HTMLAttributes } from "react";
 type ModalBodyProps = HTMLAttributes<HTMLDivElement>;
 
 export const ModalBody = ({ children, ...rest }: ModalBodyProps) => {
-  return <main {...rest}>{children}</main>;
+  return (
+    <main className="flex gap-3" {...rest}>
+      {children}
+    </main>
+  );
 };
